8020d99059c74536715bbf06491bd753b4e5ca6e
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-03-25 01:50:52 -0400

    proj

---
+++ portfolioproject/src/components/SideBarData.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+
+export const SideBarData = [
+{
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+},
+{
+    title: 'Reports',
+    path: '/reports',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+},
+{
+    title: 'Products',
+    path: '/products',
+    icon: <FaIcons.FaCartPlus />,
+    cName: 'nav-text'
+},
+{
+    title: 'Support',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: 'nav-text'
+},
+]
