48ad49cb9432a68f763fb4f5c2a2ba9643037485
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-04-30 07:05:41 -0400

    update

---
+++ login/components/Home.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+            <FlatList
+                keyExtractor={(item) => item.id}
+                data={(excerises)}
+                renderItem={({ item }) => (
+                    <Text style={styles.item}>{item.name}</Text>
+                )
+                }
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
